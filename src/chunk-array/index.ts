c5d88d03bb567ae37fe14c21edf8d0a4516d49df
Sa4ok <sasha.rusinka@gmail.com>
2025-05-19 16:06:46 +0300

    completed task#2

--- src/chunk-array/index.ts
+++ src/chunk-array/index.ts
@@ -26,5 +26,13 @@ import { ChunkArrayFn } from './types';
  * []
  */
 export const chunkArray: ChunkArrayFn = (arr, size) => {
-  throw new Error('Not Implemented');
+  if (size <= 0) {
+    return [];
+  }
+
+  const res = [];
+  for (let i = 0; i < arr.length; i += size) {
+    res.push(arr.slice(i, i + size));
+  }
+  return res;
 };
